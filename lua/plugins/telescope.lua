6de638bab608c558a4cb8a4294d909a9ce7f4d49
Lachlan Hugo <lachlan.hugo@srcentre.com.au>
2025-06-20 12:43:17 +1000

    Add copilot chat

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,11 +4,18 @@ return {
     branch = "0.1.x",
 
     dependencies = {
-        "nvim-lua/plenary.nvim"
+        "nvim-lua/plenary.nvim",
+        "nvim-telescope/telescope-ui-select.nvim"
     },
 
     config = function()
-        require('telescope').setup({})
+        require('telescope').setup({
+            extensions = {
+                ["ui-select"] = { require("telescope.themes").get_dropdown {} }
+            }
+        })
+
+        require('telescope').load_extension('ui-select')
 
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "Telescope all files" })
