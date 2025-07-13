a98ed0f430853ecd8936b3233e9dc9b8c136e160
Lachlan Hugo <lachlan.hugo@srcentre.com.au>
2025-07-14 08:45:58 +1000

    Update remap.lua

--- lua/lachlangh/remap.lua
+++ lua/lachlangh/remap.lua
@@ -22,3 +22,15 @@ vim.api.nvim_set_keymap('t', '<A-l>', '<C-\\><C-N><C-w>l', { desc = "Navigate to
 -- quickfix movements
 vim.keymap.set("n", "<leader>qn", "<Cmd>cnext<CR>", { desc = "Move to the next quickfix item" })
 vim.keymap.set("n", "<leader>qp", "<Cmd>cprev<CR>", { desc = "Move to the previous quickfix item" })
+
+-- Splitting & Resizing
+vim.keymap.set("n", "<leader>sv", ":vsplit<CR>", { desc = "Split window vertically" })
+vim.keymap.set("n", "<leader>sh", ":split<CR>", { desc = "Split window horizontally" })
+vim.keymap.set("n", "<C-Up>", ":resize +2<CR>", { desc = "Increase window height" })
+vim.keymap.set("n", "<C-Down>", ":resize -2<CR>", { desc = "Decrease window height" })
+vim.keymap.set("n", "<C-Left>", ":vertical resize -2<CR>", { desc = "Decrease window width" })
+vim.keymap.set("n", "<C-Right>", ":vertical resize +2<CR>", { desc = "Increase window width" })
+
+-- Better indenting in visual mode
+vim.keymap.set("v", "<", "<gv", { desc = "Indent left and reselect" })
+vim.keymap.set("v", ">", ">gv", { desc = "Indent right and reselect" })
