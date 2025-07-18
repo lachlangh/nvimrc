90ebb4e332a139a363b2d3911fca4444be80e52f
Lachlan Hugo <lachlan@lhugo.dev>
2025-07-18 17:39:28 +1000

    Add JS lsp configs

--- lua/lachlangh/lsp.lua
+++ lua/lachlangh/lsp.lua
@@ -2,5 +2,8 @@ vim.lsp.enable {
     "lua_ls",
     "r_language_server",
     "air_r_formatter",
-    "rust_analyzer"
+    "rust_analyzer",
+    "eslint",
+    "tsserver",
+    "emmet_language_server",
 }
