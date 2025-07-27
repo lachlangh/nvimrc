9c745cd1766332f4eafd59823318af645c0d55aa
Lachlan Hugo <lachlan@lhugo.dev>
2025-07-27 10:49:02 +1000

    Fix auto fold behaviour with startup plugin

--- lua/plugins/startup.lua
+++ lua/plugins/startup.lua
@@ -3,13 +3,42 @@ return {
     dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim", "nvim-telescope/telescope-file-browser.nvim" },
     config = function()
         require "startup".setup({ theme = "dashboard" })
-        -- for 'startup' filetype we need to disable folds
+
+        -- create an augroup so we can clear/reload safely
+        local fold_grp = vim.api.nvim_create_augroup("StartupFolding", { clear = true })
+
+        -- when the dashboard opens, stash the window’s fold settings and disable folding
         vim.api.nvim_create_autocmd("FileType", {
+            group = fold_grp,
             pattern = "startup",
             callback = function()
-                vim.wo.foldenable = false
+                vim.b.startup_old = {
+                    method = vim.wo.foldmethod,
+                    enable = vim.wo.foldenable,
+                    level  = vim.wo.foldlevel,
+                }
+                vim.b.is_startup  = true
+
                 vim.wo.foldmethod = "manual"
+                vim.wo.foldenable = false
+                vim.wo.foldlevel  = 99
             end,
         })
-    end
+
+        -- when you leave that buffer, restore the previous fold settings
+        vim.api.nvim_create_autocmd("BufWinLeave", {
+            group = fold_grp,
+            pattern = "*",
+            callback = function()
+                if vim.b.is_startup and vim.b.startup_old then
+                    local old         = vim.b.startup_old
+                    vim.wo.foldmethod = old.method
+                    vim.wo.foldenable = old.enable
+                    vim.wo.foldlevel  = old.level
+                    vim.b.is_startup  = nil
+                    vim.b.startup_old = nil
+                end
+            end,
+        })
+    end,
 }
